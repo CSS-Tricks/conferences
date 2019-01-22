b65048ff628b3764a864ec6ab9038ce2987f7ed2
Chris Coyier <chriscoyier@gmail.com>
2019-01-22 08:47:51 -0800

    css working

--- .eleventy.js
+++ .eleventy.js
@@ -3,12 +3,12 @@ module.exports = function(config) {
     return collection.getFilteredByGlob("site/conferences/*.md");
   });
 
+  config.addPassthroughCopy("css");
+
   return {
-    dir: {
-      input: "site",
-      output: "dist"
-    },
-    templateFormats: ["njk", "md"],
+    dir: { input: "site", output: "dist", includes: "_includes" },
+    passthroughFileCopy: true,
+    templateFormats: ["njk", "md", "css"],
     htmlTemplateEngine: "njk",
     markdownTemplateEngine: "njk"
   };
