90e5f354f8fb39e278e5d5ccfd25bfb0a1571334
Chris Coyier <chriscoyier@gmail.com>
2019-02-22 07:08:03 -0800

    prettier

--- site/script/index.js
+++ site/script/index.js
@@ -1,12 +1,12 @@
-const viewButtons = document.querySelectorAll(".style-switcher button");
+const viewButtons = document.querySelectorAll('.style-switcher button');
 
 viewButtons.forEach(button => {
-  button.addEventListener("click", el => {
-    document.body.classList.remove("grid", "list");
+  button.addEventListener('click', el => {
+    document.body.classList.remove('grid', 'list');
     viewButtons.forEach(button => {
-      button.classList.remove("active");
+      button.classList.remove('active');
     });
-    el.target.classList.add("active");
+    el.target.classList.add('active');
     document.body.classList.add(el.target.dataset.bodyClass);
   });
 });
