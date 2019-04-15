f1f9a801ecc5bceba82f469d6f54ab7220a63002
Mat Marquis <mat@matmarquis.com>
2019-04-15 14:42:07 -0400

    Persist view setting across page reloads.

--- site/script/index.js
+++ site/script/index.js
@@ -2,11 +2,24 @@ const viewButtons = document.querySelectorAll('.style-switcher button');
 
 viewButtons.forEach(button => {
   button.addEventListener('click', el => {
+    var bodyClass = el.target.dataset.bodyClass;
     document.body.classList.remove('grid', 'list');
     viewButtons.forEach(button => {
       button.classList.remove('active');
     });
     el.target.classList.add('active');
     document.body.classList.add(el.target.dataset.bodyClass);
+    localStorage.setItem('view', bodyClass);
   });
 });
+
+
+(function() {
+  var savedState = localStorage.getItem('view'),
+    current;
+
+   if( savedState !== null ) {
+     current = document.querySelector('[data-body-class="' +  savedState + '"]');
+     current.click();
+   }
+}());
