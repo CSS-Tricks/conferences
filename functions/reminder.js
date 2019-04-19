3c290ff21d7793f4f6733382d8687d0b6d9f02a2
Mat Marquis <mat@matmarquis.com>
2019-04-19 17:33:09 -0400

    Initial work on Sparkpost-powered “send me a reminder” Netlify function.

---
+++ functions/reminder.js
@@ -0,0 +1,41 @@
+/*
+exports.handler = function(event, context, callback) {
+  const SparkPost = require('sparkpost');
+  const client = new SparkPost( process.env.SPARKPOST );
+  const params = event.queryStringParameters;
+
+  client.transmissions.send({
+    options: {
+      sandbox: true
+    },
+    content: {
+      from: 'testing@sparkpostbox.com',
+      subject: 'Hello, World!',
+      html:`<html><body><p>Hi. This seems to be working.</p></body></html>`
+    },
+    recipients: [
+      {
+        address: 'mat@matmarquis.com'
+      }
+    ]
+  })
+  .then(data => {
+    console.log('Woohoo! You just sent your first mailing!');
+    console.log(data);
+  })
+  .catch(err => {
+    console.log('Whoops! Something went wrong');
+    console.log(err);
+  });
+};*/
+
+exports.handler = function(event, context, callback) {
+  const SparkPost = require('sparkpost');
+  const client = new SparkPost( process.env.SPARKPOST );
+  
+
+  callback(null, {
+    statusCode: 200,
+    body: `<h1>${ params.email }</h1><p>${ params.lede }</p>`
+  });
+};
