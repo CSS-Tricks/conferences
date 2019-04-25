01edd4d2fa947123b8306a0e00ca779295b85876
Chris Coyier <chriscoyier@gmail.com>
2019-04-25 14:43:25 -0700

    focus the input; just one iife

--- site/script/index.js
+++ site/script/index.js
@@ -1,74 +1,70 @@
-const viewButtons = document.querySelectorAll('.style-switcher button');
-
-viewButtons.forEach(button => {
-  button.addEventListener('click', el => {
-    var bodyClass = el.target.dataset.bodyClass;
-    document.body.classList.remove('grid', 'list');
-    viewButtons.forEach(button => {
-      button.classList.remove('active');
-    });
-    el.target.classList.add('active');
-    document.body.classList.add(el.target.dataset.bodyClass);
-    localStorage.setItem('view', bodyClass);
-  });
-});
-
-
 (function() {
+  // VIEW SWITCHER
+  const viewButtons = document.querySelectorAll('.style-switcher button');
+
+  viewButtons.forEach(button => {
+    button.addEventListener('click', el => {
+      var bodyClass = el.target.dataset.bodyClass;
+      document.body.classList.remove('grid', 'list');
+      viewButtons.forEach(button => {
+        button.classList.remove('active');
+      });
+      el.target.classList.add('active');
+      document.body.classList.add(el.target.dataset.bodyClass);
+      localStorage.setItem('view', bodyClass);
+    });
+  });
+
   var savedState = localStorage.getItem('view'),
     current;
 
-   if( savedState !== null ) {
-     current = document.querySelector('[data-body-class="' +  savedState + '"]');
-     current.click();
-   }
-}());
+  if (savedState !== null) {
+    current = document.querySelector('[data-body-class="' + savedState + '"]');
+    current.click();
+  }
 
-(function() {
+  // EMAIL BUTTONS
   var toggles = document.querySelectorAll('[aria-controls]');
 
   toggles.forEach(el => {
-    el.addEventListener( "click", function( e ){
-      let target = this.getAttribute( "aria-controls" ),
-        targetEl = document.querySelector( '#' + target ),
-        newState = this.getAttribute( 'aria-expanded' ) === "true" ? false : true;
+    el.addEventListener('click', function(e) {
+      let target = this.getAttribute('aria-controls'),
+        targetEl = document.querySelector('#' + target),
+        newState = this.getAttribute('aria-expanded') === 'true' ? false : true;
 
-      this.setAttribute( "aria-expanded", newState );
-      targetEl.setAttribute( "aria-hidden", !newState );
+      this.setAttribute('aria-expanded', newState);
+      targetEl.setAttribute('aria-hidden', !newState);
+      targetEl.querySelector('input[type=email]').focus();
     });
   });
-}());
 
-(function() {
   var forms = document.querySelectorAll('form');
 
   forms.forEach(el => {
-    el.addEventListener( "submit", function( e ){
+    el.addEventListener('submit', function(e) {
       let form = this,
-        action = form.getAttribute( "action" ),
-        method = form.getAttribute( "method" ),
-        data   = new FormData( form );
+        action = form.getAttribute('action'),
+        method = form.getAttribute('method'),
+        data = new FormData(form);
 
-      fetch(
-        action,
-        {
-          method: method,
-          body: data
-        }
-      )
-      .then( response => {
-        if( response.ok === true ) {
-          let submit = form.querySelector( ".send-email" );
-
-          submit.setAttribute( "aria-label", "Sent!" );
-          submit.querySelector( 'use' ).setAttribute( 'xlink:href', '#icon-check');
-        } else {
-          console.error('error: no response');
-        }
+      fetch(action, {
+        method: method,
+        body: data
       })
-      .catch( error => console.error('error:', error) );
+        .then(response => {
+          if (response.ok === true) {
+            let submit = form.querySelector('.send-email');
+
+            submit.setAttribute('aria-label', 'Sent!');
+            submit
+              .querySelector('use')
+              .setAttribute('xlink:href', '#icon-check');
+          } else {
+            console.error('error: no response');
+          }
+        })
+        .catch(error => console.error('error:', error));
       e.preventDefault();
     });
   });
-}());
-
+})();
