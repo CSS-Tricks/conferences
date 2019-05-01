0ce5868faf31287deeedda7016504628e365af1b
Mat Marquis <mat@matmarquis.com>
2019-05-01 11:19:39 -0400

    Let's stick with just the conference title for now.

--- functions/send-email/send-email.js
+++ functions/send-email/send-email.js
@@ -22,20 +22,9 @@ exports.handler = function(event, context, callback) {
       content: {
         from: 'chris@css-tricks.com',
         subject: `${conf}`,
-        html: `<html>
-  <body>
-    <h1 style="margin-bottom:0"><a href="${url}">${conf}</a></h1>
-    <h2 style="margin:.15em">${loc}</h2>
-    <p style="margin-top:0;font-weight:bold">${dates}</p>
-
-    ${desc}
-
-    <p><a href="${url}">${conf} →</a></p>
-    <p><a href="${coc}" style="font-size:.8em;">Code of Conduct</a></p>
-  </body>
-</html>`
+        html: `none`
       },
-      recipients: [{ address: email }]
+      recipients: [{ address: 'mat@matmarquis.com' }]
     })
     .then(data => {
       callback(null, {
