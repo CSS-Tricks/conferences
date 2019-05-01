9afece3073fcd7fcf20f3e605a77970fc797c8e1
Mat Marquis <mat@matmarquis.com>
2019-05-01 12:47:02 -0400

    I think I was sending malformed data to begin with.

--- functions/send-email/send-email.js
+++ functions/send-email/send-email.js
@@ -1,54 +1,52 @@
 const SparkPost = require('sparkpost');
 const client = new SparkPost(process.env.SPARKPOST);
-const { parse } = require('querystring');
-
-function collectRequestData(request, callback) {
-    const FORM_URLENCODED = 'application/x-www-form-urlencoded';
-
-    if(request.headers['content-type'] === FORM_URLENCODED) {
-        let body = '';
-        request.on('data', chunk => {
-            body += chunk.toString();
-        });
-        request.on('end', () => {
-            callback(parse(body));
-        });
-    }
-    else {
-        callback(null);
-    }
-}
+const querystring = require('querystring');
 
 exports.handler = function(event, context, callback) {
   const post = querystring.parse(event.body);
+
+  const email = post['email'],
+    conf = post['conf'],
+    url = post['url'],
+    loc = post['loc'],
+    dates = post['dates'],
+    desc = post['desc'],
+    coc = post['coc'];
 
   if (!post || event.httpMethod !== 'POST') {
     return;
   }
 
-  collectRequestData(req, result => {
-    client.transmissions
-      .send({
-        content: {
-          from: 'chris@css-tricks.com',
-          subject: `${ result.conf }`,
-          html: `test`
-        },
-        recipients: [{ address: `${ result.email }` }]
-      })
-      .then(data => {
-        callback(null, {
-          statusCode: 200,
-          body: `Message sent!`
-        });
-      })
-      .catch(err => {
-        callback(null, {
-          statusCode: 500,
-          body: `Sorry, something went wrong. ${err}`
-        });
-    });
-  });
+  client.transmissions
+    .send({
+      content: {
+        from: 'chris@css-tricks.com',
+        subject: `${conf}`,
+        html: `<html>
+  <body>
+    <h1 style="margin-bottom:0"><a href="${url}">${conf}</a></h1>
+    <h2 style="margin:.15em">${loc}</h2>
+    <p style="margin-top:0;font-weight:bold">${dates}</p>
 
- 
+    ${desc}
+
+    <p><a href="${url}">${conf} →</a></p>
+    <p><a href="${coc}" style="font-size:.8em;">Code of Conduct</a></p>
+  </body>
+</html>`
+      },
+      recipients: [{ address: email }]
+    })
+    .then(data => {
+      callback(null, {
+        statusCode: 200,
+        body: `Message sent!`
+      });
+    })
+    .catch(err => {
+      callback(null, {
+        statusCode: 500,
+        body: `Sorry, something went wrong. ${err}`
+      });
+    });
 };
