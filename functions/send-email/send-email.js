51b2f9059d2f236243af3f43a9cd1c82244d168d
Mat Marquis <mat@matmarquis.com>
2019-05-01 11:04:37 -0400

    Re-adding POST parsing, but not using it beyond the subject line.

--- functions/send-email/send-email.js
+++ functions/send-email/send-email.js
@@ -3,11 +3,25 @@ const client = new SparkPost(process.env.SPARKPOST);
 const querystring = require('querystring');
 
 exports.handler = function(event, context, callback) {
+  const post = querystring.parse(event.body);
+
+  const email = post['email'],
+    conf = post['conf'],
+    url = post['url'],
+    loc = post['loc'],
+    dates = post['dates'],
+    desc = post['desc'],
+    coc = post['coc'];
+
+  if (!post || event.httpMethod !== 'POST') {
+    return;
+  }
+
   client.transmissions
     .send({
       content: {
         from: 'chris@css-tricks.com',
-        subject: `conf`,
+        subject: `${conf}`,
         html: `testing`
       },
       recipients: [{ address: 'mat@matmarquis.com' }]
